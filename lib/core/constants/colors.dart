93f17cb90a393a35b40fb78b88e46e0a0cd00e1f
George <zhorenty@gmail.com>
2023-03-03 18:23:38 +0300

    color changing

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ColorConstants {
-  static const Color primaryColor = Color(0xff1B1AFF);
+  static const Color primaryColor = Color.fromARGB(255, 62, 88, 255);
   static const Color secondaryWhiteColor = Color(0xffffffff);
   static const Color secondaryBlackColor = Color(0xff1c1c1c);
 }
